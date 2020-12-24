c6dd0ccb1d64a351637091ecb0503657069ef4d3
wangyinjia <1603068044@qq.com>
2020-12-24 23:04:05 +0800

    提交

--- env.js
+++ env.js
@@ -1,12 +1,12 @@
 // .env.js 文件
 const ENV_API_URL = {
-	// development: 'https://dev.smtsoft.cn/api/esp/portal', //开发环境
-	development: 'https://dev.smtsoft.cn/api/esp/portal', //开发环境
-	production: 'https://cloud.smtsoft.cn/api/esp/portal', //生产环境
+	// development: 'https://', //开发环境
+	development: 'https://', //开发环境
+	production: 'https://', //生产环境
 }
 const ENV_RESOURCE_URL = {
-	development: 'https://smt-cloud-esp-files.oss-cn-shanghai.aliyuncs.com/mall', //开发环境
-	production: 'https://smt-cloud-esp-files.oss-cn-shanghai.aliyuncs.com/mall', //生产环境
+	development: 'https://', //开发环境
+	production: 'https://', //生产环境
 }
 export const API_URL = "http://localhost:8080"; //后台接口域名
 
