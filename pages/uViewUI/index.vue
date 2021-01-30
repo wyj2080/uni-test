5d22fb95d17086820d8492650dbe8f6bb04e9453
wangyinjia <1603068044@qq.com>
2021-01-31 02:19:45 +0800

    uViewUI内置样式

--- pages/uViewUI/index.vue
+++ pages/uViewUI/index.vue
@@ -1,5 +1,29 @@
 <template>
 	<view class="container">
+		<u-divider>内置样式(class)</u-divider>
+		<view class="u-line-1">
+			字数过长自动点点点，u-line-1，字数过长自动点点点，u-line-1，字数过长自动点点点
+		</view>
+		<view class="u-relative u-font-20">
+			相对定位(u-relative或u-rela)
+			<view class="u-absolute" style="top: 0px;right: 0px;">绝对定位(u-absolute或u-abso)</view>
+		</view>
+		<view class="u-text-center" style="display: inline-block;width: 200px;border: 1px solid #000;">文字对齐(u-text-center)</view>
+		<button class="u-reset-button" @tap="btnClick">重置按钮样式u-reset-button</button>
+		<view class="u-p-20" style="border: 1px solid #000;">padding：u-p-20或u-padding-20</view>
+		<view style="display: flex;">
+			<view class="u-font-20">字体20r</view>
+			<view class="u-font-24">字体24r</view>
+			<view class="u-font-28">字体28r</view>
+			<view class="u-font-32">字体32r</view>
+			<view class="u-font-36">字体36r</view>
+		</view>
+		<button class="u-reset-button">重置按钮样式</button>
+		<u-modal v-model="show" content="点击确定进行授权">
+			<button open-type="getUserInfo" class="u-reset-button" slot="confirm-button" @getuserinfo="getuserinfo">确定</button>
+		</u-modal>
+		<u-button @click="show = true">打开modal</u-button>
+		
 		<u-time-line>
 			<u-time-line-item>
 				<!-- 此处没有自定义左边的内容，会默认显示一个点 -->
@@ -18,11 +42,16 @@
 	export default {
 		data() {
 			return {
-				
+				show: false
 			}
 		},
 		methods: {
-			
+			getuserinfo(res) {
+				console.log(res);
+			},
+			btnClick(){
+				alert('这是按钮')
+			}
 		}
 	}
 </script>
